46034e62a9efe2e6c7cd0f9de82561bd23b94665
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2019-07-17 09:11:45 +0530

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,67 +1,36 @@
-(function()  {
-    let d3Script = document.createElement('script');
-    d3Script.src = 'https://d3js.org/d3.v5.min.js';
-    d3Script.async = false;
-    document.head.appendChild(d3Script);
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<style>
+		:host {
+			border-radius: 25px;
+			border-width: 4px;
+			border-color: black;
+			border-style: solid;
+		} 
+		</style> 
+	`;
 
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-    <style>
-    :host {
-        border-radius: 25px;
-        border-width: 4px;
-        border-color: black;
-        border-style: solid;
-    } 
-    </style> 
-    `;
-
-    d3Script.onload = () => 
-
-    customElements.define('com-infy-colorbox-solution', class Gauge extends HTMLElement {
-
-
-        disconnectedCallback () {
-            // your cleanup code goes here
-            try{
-                document.head.removeChild(d3Script);
-            }
-            catch{}
-            }
-    
-        constructor() {
-            super();
-            //Constants
-            if (!window._d3){
-                window._d3 = d3;
-            }
-            
-            this._shadowRoot = this.attachShadow({mode: 'open'});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this.style.height = "100%";  //Beta Workaround
-            //this._svgContainer;
-    
-     
-
-            //Adding event handler for click events
-            this.addEventListener("click", event => {
+	class ColoredBox extends HTMLElement {
+		constructor() {
+			super();
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.style.height = "100%";
+			this.addEventListener("click", event => {
 				var event = new Event("onClick");
 				this.dispatchEvent(event);
 			});
-        };
+		}
 
-        //Getters and Setters
-        set color(newColor) {
+		set color(newColor) {
 			this.style["background-color"] = newColor;
 		}
 
 		get color() {
 			return this.style["background-color"];
 		}
+	}
 
-
-    
-    
-    });
-    customElements.define("com-infy-colorbox-solution", ColoredBox);   
+	customElements.define("com-sap-sample-coloredbox", ColoredBox);
 })();
